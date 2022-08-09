f167e3f0c48b8cf795c4cd4690db0cbb7fbb2b81
Kiran Banger <kiranb004@gmail.com>
2022-08-09 16:36:24 -0700

    adds code for sign in functinality

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const userController = require('./userController.js');
 
 //app.use(express.json()); //what is thisused for?
 app.use(express.urlencoded({extended: true})); // extended: true gets rid of a warning for body-parser
-// without express.urlencoded, req.body is just {}
+// without express.urlencoded(), req.body is just {}
 
 app.get('/', (req, res) => {
   return res.status(200).sendFile(path.resolve(__dirname, '../index.html'));
@@ -20,6 +20,25 @@ app.post('/', userController.createUser,(req, res) =>{
   res.sendStatus(200);
 })
 // handle rquest to login button
+
+
+// catch all route handler
+app.use( (req, res) => res.status(404).send('We cannot find th page you are looking for.'))
+
+
+// global error handler
+app.use((err, req, res, next)=>{
+  const defaultError = {
+    log: 'Unknown middleware error.',
+    status: 500,
+    message: {err: 'There is a problem.'}
+  };
+
+  const errorObj = Object.assign({}, defaultError, err)
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+})
+
 app.listen(PORT, () => console.log(`Listening on port ${PORT}.`));
 
 module.exports = app;
--- server/userController.js
+++ server/userController.js
@@ -1,9 +1,30 @@
 //  has password 
 // insert email and hashed passowrd into database
+const db = require('../server/models/userModels.js'); // db.query(text, params, callback) to run a query
+const bcrypt = require('bcrypt');
 
 const userController = {};
 
-userController.createUser = (req, res) => {
+userController.createUser = (req, res, next) => {
+  const { email } = req.body;
+  const saltRounds = 2; //rounds used to generate the salt
+
+  console.log('in create user!!!',req.body)
+  bcrypt.hash(req.body.password, saltRounds)
+  .then( hash => {
+    console.log(hash)
+
+    const q = `insert into users (email, hashcode) values ($1,$2) returning *`
+    db.query(q, [email, hash])
+    .then(dbRes => {
+      if(dbRes.rowCount === 1){
+        // redirect to a signed in page
+        // redirect here or in server.js?
+      }
+    })
+  })
+    .then( () => next() )
+    .catch(error => next({message: {err: 'Error in userController.createUser!'}, log: error}))
 
 }
 
